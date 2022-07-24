be222eb9123355a4f7de488475031132a929d6be
Igor Shakirov <igorshakirov01101996@gmail.com>
2022-07-24 16:17:49 +0300

    Kata_PP_3.1.1_V_1.1.1 add @Valid

--- src/main/java/org/example/models/User.java
+++ src/main/java/org/example/models/User.java
@@ -17,10 +17,8 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
-    @NotEmpty(message = "Last Name should not be empty")
     @Size(min = 2, max = 30, message = "Last Name should be between 2 and 30 characters")
     private String lastName;
     @Min(value = 0, message = "The age must be greater than 0")
